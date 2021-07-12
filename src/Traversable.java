20bcb58dbdbbd114a5ba8d513400b3e12dae61cf
AlmogMen9 <58885455+AlmogMenashe@users.noreply.github.com>
2021-07-12 18:42:25 +0300

    Add files via upload

---
+++ src/Traversable.java
@@ -0,0 +1,11 @@
+import java.util.Collection;
+
+/**
+ * This interface defines the functionality required for a traversable graph
+ */
+public interface Traversable<T> {
+    public Node<T> getOrigin();
+    public int getValue(T index);
+    //todo: check about includeDiagonal
+    public Collection<Node<T>>  getReachableNodes(Node<T> someNode,boolean includeDiagonal);
+}
